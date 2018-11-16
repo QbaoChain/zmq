6213e4b488d42db2237dfc1aae970540a0422502
hepengfei <hepf33@hotmail.com>
2018-11-16 18:14:23 +0800

    change logic to retrieve address balance

--- src/main/java/com/aethercoder/dao/AddressInfoDao.java
+++ src/main/java/com/aethercoder/dao/AddressInfoDao.java
@@ -3,8 +3,10 @@ package com.aethercoder.dao;
 import com.aethercoder.entity.AddressInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -20,5 +22,6 @@ public interface AddressInfoDao extends JpaRepository<AddressInfo, Long>{
     @Query(value = "select DISTINCT(block_height) from t_address_info ORDER BY block_height asc", nativeQuery = true)
     List<BigInteger> getAllBlockHeightFromDB();
 
-
+    @Query(value = "select sum(balance_change) from t_address_info where address = :address", nativeQuery = true)
+    BigDecimal findSumBalanceChange(@Param("address") String address);
 }
