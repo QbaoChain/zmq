77e2df13229c2ecc6ed9b8c0c1408752bdacaf7f
hepengfei <hepf33@hotmail.com>
2018-11-16 18:14:53 +0800

    remove useless import

--- src/main/java/com/aethercoder/dao/TokenInfoDao.java
+++ src/main/java/com/aethercoder/dao/TokenInfoDao.java
@@ -1,14 +1,10 @@
 package com.aethercoder.dao;
 
 import com.aethercoder.entity.TokenInfo;
-import com.aethercoder.entity.TxInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by king on 20/09/2018.
